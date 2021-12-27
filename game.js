f82ac998d3e9eeb53f84694b19afd8ff3ea39b3c
BarSh0 <93907409+BarSh0@users.noreply.github.com>
2021-12-27 23:44:23 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,24 +1,45 @@
 class Item {
+
+
     constructor() {
       // weapon||trap||potion
-      this._itemname = null ; 
+      this._itemname = itemsNames[randomName] ; 
+
       //?
       this._droprate = Math.floor(Math.random() * 10) + 1; ;
+
       // health||attack||defence
-      this._ability = null; 
+      this._ability =abilities[randomAbilty]; 
+      
       // -20 to 20
       this._amount = Math.floor(Math.random() * 40) - 20; 
     }
+   
     show() {
       return 'I have a ' + this.itemname;
     }
   }
+  var itemsNames = [
+    'weapon',
+    'trap',
+    'potion'
+];
+var randomName = Math.floor(Math.random()*itemsNames.length);
 
-  function genertor() {
-    let items;
-   for (let i=0;i<10;i++){
-     items[i]= new Item();
-   }
-   console.log(items) ;
-  }
-  genertor()
+var abilities = [
+  'health',
+  'attack',
+  'defence'
+];
+var randomAbilty = Math.floor(Math.random()*abilities.length);
+
+
+
+  // function genertor() {
+  //   let items;
+  //  for (let i=0;i<10;i++){
+  //    items[i]= new Item();
+  //  }
+  //  console.log(items) ;
+  // }
+  // genertor()
