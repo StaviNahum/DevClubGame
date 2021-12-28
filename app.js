7c66be9926652e82913b90d84b77f70b9af018b4
Bar <barshoshani274@gmail.com>
2021-12-28 15:34:31 +0200

    update app

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ import { GlobalKeyboardListener } from "node-global-key-listener";
 
 const NUM_OF_ENEMIES = 10;
 const v = new GlobalKeyboardListener();
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-//   });
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
 function generateArr(arrType) {
     let arr = [];
@@ -58,52 +58,52 @@ let board = new Board(enemies, items, user);
 
 console.log('~*~THE GAME~*~');
 console.log('___________');
-console.log('--- Press ENTER for start ---');
-    v.addListener((e) => {
-      if (e.name === "RETURN") {
-
-        board.printBoard();
-    }
-    });
-}
-     
-// rl.question('What is your name ? ', function (name) {
-//     user.setName(name);
-//     console.log(`Hello ${name}, Welcome To The GAME!`)
+// console.log('--- Press ENTER for start ---');
+//     v.addListener((e) => {
+//       if (e.name === "RETURN") {
+//         console.log('--- Welcome ! ---');
+//         board.printBoard();
+//     }
 //     });
-//     while(!user.dead()&&user.possion.x!=size&&user.possion.y!=size){ //need to fix
-//         openmenu();
-//         rl.question('What is your move ? ', function (move) {
-//             board.move(move); // ??
-//         });
-//         let position =board[user.possion.x][user.possion.y];
-//             if(position instanceof Item){
-//                 position.show();
-//                 if(position.name=='Trap')
-//                     board.trap(position);
-//                     else{
-//                         rl.question('Do you want to grab it(yes or no)? ', function (answer) {
-//                             if(answer=='yes'){
-//                                 user.grab(board[user.possion.x][user.possion.y]);
-//                             }
-//                         });
-//                     }
-//                       rl.close();
-//                 }
-//             if(position instanceof Enemy){
-//                 fight(user,position);
-//             }
-//         }
-//             if(user.dead())
-//             console.log('You Are Dead!');
-//             if(user.possion.x==size&&user.possion.y==size) //need to fix
-//             console.log('You Win!');
 
-//   rl.on('close', function () {
-//     console.log('\nBYE BYE !!!');
-//     process.exit(0);
-//   });
-
-
-
+rl.question('What is your name ? ', function (name) {
+        user.setName(name);
+        console.log(`Hello ${name}, Welcome To The GAME!`)
+        });
+        while(!user.dead()&&user.possion.x!=size&&user.possion.y!=size){ //need to fix
+            openmenu();
+            rl.question('What is your move ? ', function (move) {
+                    board.move(move); // ??
+        });
+        let position =board[user.possion.x][user.possion.y];
+            if(position instanceof Item){
+                    position.show();
+                    if(position.name=='Trap')
+                        board.trap(position);
+                        else{
+                                rl.question('Do you want to grab it(yes or no)? ', function (answer) {
+                                        if(answer=='yes'){
+                                                user.grab(board[user.possion.x][user.possion.y]);
+                            }
+                        });
+                    }
+                      rl.close();
+                }
+            if(position instanceof Enemy){
+                    fight(user,position);
+                }
+            }
+                if(user.dead())
+                console.log('You Are Dead!');
+                if(user.possion.x==size&&user.possion.y==size) //need to fix
+                console.log('You Win!');
+    
+      rl.on('close', function () {
+            console.log('\nBYE BYE !!!');
+            process.exit(0);
+          });
+    }
+        
+        
+        
 main();
